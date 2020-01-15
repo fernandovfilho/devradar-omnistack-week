8b180bcc3ece591b8514c91020e2e0282ee8d4c9
Fernando Vargas <fernandov.filho@gmail.com>
2020-01-15 12:55:50 -0300

    Terceira aula da semana Omnistack 10

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -11,6 +12,7 @@ mongoose.connect(`mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASSW
     useCreateIndex: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
