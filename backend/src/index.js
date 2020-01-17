2165f0c5288814b31c3bd5fdfd52736698d069fc
Fernando Vargas <fernandov.filho@gmail.com>
2020-01-17 12:31:09 -0300

    Added: Maps page

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const http = require('http');
 const cors = require('cors');
 require('dotenv').config();
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(`mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@cluster0-ovbaw.mongodb.net/${ process.env.DB_NAME }?retryWrites=true&w=majority`,{
     useNewUrlParser: true,
@@ -17,6 +22,6 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(process.env.SERVER_PORT, () => {
+server.listen(process.env.SERVER_PORT, () => {
     console.log(`Server listening at port: ${ process.env.SERVER_PORT }`);
 });
